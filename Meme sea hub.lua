097b3330ed4adb9376634ee1d1f7a4c9b9c2b823
oi123-lab <ggggprowar@gmail.com>
2025-05-03 16:56:34 -0300

    Update Meme sea hub.lua

--- Meme sea hub.lua
+++ Meme sea hub.lua
@@ -1,164 +1,28 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
-local Window = redzlib:MakeWindow({
-  Title = "MATRIX HUB v0.1 : MEME SEA",
-  SubTitle = "by MATRIX COMMUNITY",
-  SaveFolder = "MatrixHubConfigs"
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "CLT HUB : By Soy El Torrada", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
+
+local Tab = Window:MakeTab({
+	Name = "BEM VINDO",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
 })
 
-local Tabs = {
-  Main = Window:AddTab({ Title = "Main", Icon = "house" }),
-  Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
-}
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
 
-Tabs.Main:AddSection("Auto farm")
 
-Tabs.Main:AddDropdown("Fighting style", {
-  Title = "Fighting style",
-  Values = {"Melee", "Weapons"},
-  Multi = false,
-  Default = "Melee",
-  Callback = function(Value)
-    
-    getgenv().EquipStyle = Value
-    
-  end
-})
-
-Tabs.Main:AddSlider("Player height", {
-  Title = "Player height",
-  Description = "",
-  Default = 7,
-  Min = 5,
-  Max = 15,
-  Rounding = 10,
-  Callback = function(Value)
-    getgenv().HeightPlayer = Value
-  end
-})
-
-local Autofarmtoggle = Tabs.Main:AddToggle("Auto farm", {
-  Title = "Auto farm", 
-  Default = false,
-  Callback= function(Value)
-    RaelHubMemeSea.GetLevelAndQuest(Value)
-    RaelHubMemeSea.AutoFarm(Value)
-    
-  end
-})
-
-Tabs.Main:AddToggle("Auto click", {
-  Title = "Auto click", 
-  Default = false,
-  Callback= function(Value)
-    
-    RaelHubMemeSea.AutoClicker(Value)
-    
-  end
-})
-
-local ListMonsterDropDown
-
-Tabs.Main:AddSection("Auto farm monster selected")
-
-Tabs.Main:AddDropdown("List monster", {
-  Title = "List monster",
-  Values = GetListMonsters,
-  Multi = false,
-  Default = "...",
-  Callback = function(Value)
-    
-    ListMonsterDropDown = Value
-    
-  end
-})
-
-Tabs.Main:AddToggle("Auto farm monster selected", {
-  Title = "Auto farm monster selected", 
-  Default = false,
-  Callback= function(Value)
-    
-    if Value then
-      Autofarmtoggle:SetValue(false)
-    end
-    RaelHubMemeSea.AutoFarmMonsterSelected(ListMonsterDropDown, Value)
-    
-  end
-})
-
-local ListBossDropDown
-
-Tabs.Main:AddSection("Auto farm boss")
-
-Tabs.Main:AddDropdown("List Boss", {
-  Title = "List Boss",
-  Values = {"Giant Pumpkin", "Evil Noob", "Lord Sus"},
-  Multi = false,
-  Default = "...",
-  Callback = function(Value)
-    
-    ListBossDropDown = Value
-    
-  end
-})
-
-Tabs.Main:AddToggle("Auto farm boss summoner", {
-  Title = "Auto farm boss", 
-  Default = false,
-  Callback= function(Value)
-    
-    RaelHubMemeSea.AutoFarmBoss(ListBossDropDown, Value)
-    
-  end
-})
-
-Tabs.Main:AddSection("Pop cat click")
-
-Tabs.Main:AddToggle("Auto click cat", {
-  Title = "Auto click cat", 
-  Default = false,
-  Callback= function(Value)
-    
-    RaelHubMemeSea.AutoClickCat(Value)
-    
-  end
-})
-
-Tabs.Main:AddButton({
-  Title = "Teleport to pop cat",
-  Description = "",
-  Callback = function()
-    local FloppaIsland = workspace.Island:FindFirstChild("FloppaIsland")
-    local PlayerCharacter = game.Players.LocalPlayer.Character
-    if FloppaIsland then
-      local Popcat_Clickable = FloppaIsland:FindFirstChild("Popcat_Clickable")
-      if Popcat_Clickable then
-        local Part = Popcat_Clickable:FindFirstChild("Part")
-        
-        if Part then
-          local humanoidrootpart = PlayerCharacter:FindFirstChild("HumanoidRootPart")
-          if humanoidrootpart then
-            humanoidrootpart.CFrame = CFrame.new(Part.Position)
-          end
-        end
-      end
-    end
-  end
-})
-
-Tabs.Main:AddButton({
-  Title = "Show clicks cat",
-  Description = "",
-  Callback = function()
-    Fluent:Notify({
-      Title = "Pop cat clicks: " .. RaelHubMemeSea.ShowClickCat(),
-      Content = "",
-      SubContent = "",
-      Duration = 3
-    })
-  end
-})
-
-InterfaceManager:SetLibrary(Fluent)
-InterfaceManager:BuildInterfaceSection(Tabs.Settings)
-
-Window:SelectTab(1)
